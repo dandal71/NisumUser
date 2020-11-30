df1ca8d33ca1675364cd41d424daed89f8625fe3
Daniel. E. Dalmagro <dandal71@gmail.com>
2020-11-30 14:13:02 -0300

    -Agregado de alunos casos de prueba

---
+++ src/test/java/cl/nisum/UserControllerTest.java
@@ -0,0 +1,57 @@
+package cl.nisum;
+
+import static org.mockito.Mockito.*;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import java.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import cl.nisum.model.entity.User;
+import cl.nisum.model.service.UserService;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+	@MockBean
+    private UserService userService;
+
+    @Autowired
+    private MockMvc mockMvc;
+    
+    @Test
+    @DisplayName("GET /api/v1/user/private/list")
+    void testGetWidgetsSuccess() throws Exception {
+    	User user1 = new User();
+    	user1.setName("Nombre Test1");
+    	user1.setUsername("utest11");
+    	user1.setEmail("usertes1@nisum.cl");
+    	
+    	User user2 = new User();
+    	user1.setName("Nombre Test2");
+    	user1.setUsername("utest22");
+    	user1.setEmail("usertes2@nisum.cl");
+    	
+    	doReturn(Arrays.asList(user1, user2)).when(userService).findAll();
+    
+    	mockMvc.perform(get("/api/v1/user/private/list"))
+          .andExpect(status().isOk())
+          .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+          .andExpect(header().string(HttpHeaders.LOCATION, "/api/v1/user/private/list"))
+          .andExpect((ResultMatcher) jsonPath("$", hasSize(2)))
+          .andExpect(jsonPath("$[0].id",  is(notNullValue())))
+          .andExpect(jsonPath("$[0].name", is("Widget Name")))
+          .andExpect(jsonPath("$[0].username", is("utest11")))
+          .andExpect(jsonPath("$[0].email", is("usertes1@nisum.cl")));
+    }
+}
---
+++ src/test/java/cl/nisum/UserRepositoryTest.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package cl.nisum;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import cl.nisum.model.entity.Phone;
+import cl.nisum.model.entity.User;
+import cl.nisum.model.repository.UserRepository;
+
+/**
+ * Repository test
+ * @author Daniel E. Dalmagro
+ *
+ */
+@SpringBootTest
+//@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class UserRepositoryTest {
+
+	   
+	@Autowired
+	private UserRepository userRespository;
+			
+	
+	@Test
+	@DisplayName("El usuario fue registrado exitosamente")
+	public void saveNewUser() {
+		User user = new User();
+		user.setName("Daniel Test2");
+		user.setUsername("danieltest");
+		user.setEmail("ddalmagro@nisun.cl");
+		user.setPassword("DDD1111");
+		
+		Phone phone = new Phone();
+		phone.setCityCode("341");
+		phone.setCountryCode("54");
+		phone.setNumber("3332221112");
+		User userBD = this.userRespository.save(user);
+		
+		user.addPhone(phone);
+		
+		assertTrue(userBD.getId() != null);		
+	} 
+		
+	
+}
