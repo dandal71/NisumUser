74fb0297fab02ffb5b18345445cd391f9f13f024
Daniel. E. Dalmagro <dandal71@gmail.com>
2020-11-30 14:12:39 -0300

    -Agregado de algunos casos de prueba

--- src/test/java/cl/nisum/NisumUsersApplicationTests.java
+++ src/test/java/cl/nisum/NisumUsersApplicationTests.java
@@ -1,13 +1,22 @@
 package cl.nisum;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import cl.nisum.controller.UserController;
 
 @SpringBootTest
 class NisumUsersApplicationTests {
 
+	@Autowired
+	private UserController userController;
+	
 	@Test
 	void contextLoads() {
+		 assertTrue( userController != null);
 	}
 
 }
